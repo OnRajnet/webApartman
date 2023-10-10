a94a0b14cfa3673f7bee5d726b339f9a79c92157
Patrik Sosna <46369524+PatSosna@users.noreply.github.com>
2023-10-10 20:37:08 +0200

    Update site (#2)
    * feat: add google map
    
    * refactor: rm unused things
    
    * feat: add preact, gallery
    
    * update description
    
    * update readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), mdx(), sitemap(), preact()],
 });
